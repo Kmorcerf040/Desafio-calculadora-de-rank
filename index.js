5be0924a0ed138db260eedb83fbca3a56807d9c9
Kmorcerf040 <kmorcerf@gmail.com>
2026-05-07 11:44:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let heroi = "Dark"
-let xp = "7500"
+let xp = "750"
 
 let rank = ""
 
-if (xp < 1000) { rank = ferro }
-else if (xp >= 1001 && xp <= 2000) { rank = bronze }
-else if (xp >= 2001 && xp <= 5000) { rank = prata }
-else if (xp >= 5001 && xp <= 7000) { rank = ouro }
-else if (xp >= 7001 && xp <= 8000) { rank = platina }
-else if (xp >= 8001 && xp <= 9000) { rank = ascendente }
-else if (xp >= 9001 && xp <= 10000) { rank = imortal }
-else if (xp >= 10001) { rank = radiante }
+if (xp < 1000) { rank = "ferro" }
+else if (xp >= 1001 && xp <= 2000) { rank = "bronze" }
+else if (xp >= 2001 && xp <= 5000) { rank = "prata" }
+else if (xp >= 5001 && xp <= 7000) { rank = "ouro" }
+else if (xp >= 7001 && xp <= 8000) { rank = "platina" }
+else if (xp >= 8001 && xp <= 9000) { rank = "ascendente" }
+else if (xp >= 9001 && xp <= 10000) { rank = "imortal" }
+else if (xp >= 10001) { rank = "radiante" }
 
 console.log("O Herói de nome " + heroi + "está no nível de " + rank)
